612f5ada6539a1db55ef2ffc53d24de540891225
Francisco Javier Gaviria Sierra <fgaviria@psl.com.co>
2019-03-06 23:33:32 -0500

    Functions and scope explanation
    **Francisco Gaviria**

--- src/algorithm/math.js
+++ src/algorithm/math.js
@@ -19,6 +19,13 @@ function findAverage2(numbers) {
     average = average / numbers.length;
     return average;
 }
-
 console.log(`The average of 5 and 9 is ${findAverage(5, 9)}`);
 console.log(`The average of 10, 11, 10, 12, 10, 11, and 10 is ${findAverage2([10, 11, 10, 12, 10, 11, 10])}`);
+
+function isPrime(number) {
+    // TODO
+}
+
+function square(number) {
+    // TODO
+}
--- src/function/function.js
+++ src/function/function.js
@@ -5,8 +5,63 @@
  * but it won't do anything unless you also turn it on. 
  * 
  * Parameters vs. Arguments
- * Parameters => variable name and appears in the function declaration
- * Arguments => argument is always going to be a value and will always appear in the code when the function is called or invoked.
+ * **Parameters** => **variable name** and appears in the function declaration
+ * **Arguments** => argument is always going to be **a value** and will always appear in the code when the function is called or invoked.
  * 
  * If you don't explicitly define a return value, the function will return undefined by default.
  */
+
+/**
+ * **function declaration**
+ */
+
+/**
+ * **function expression**
+ * - When a function is stored inside a variable it's called a function expression.
+ * - Function expressions are not hoisted
+ * 
+ * **Patterns with Function Expressions**
+ * - Functions as parameters = **callback**
+ */
+// function expression
+var catSays = function (max) {
+    var catMessage = "";
+    for (var i = 0; i < max; i++) {
+        catMessage += "meow ";
+    }
+    return catMessage;
+};
+// function with one callback with parameter
+function helloCat(catSaysFunction) {
+    return "Hello cat " + catSaysFunction(2);
+}
+console.log(catSays);
+console.log(catSays(12));
+console.log(helloCat);
+console.log(helloCat(catSays));
+
+// anonymous function expression
+var doSomething = function(y) {}
+
+// named function expression
+var doSomething = function addOne(y) {}
+
+/**
+ * **anonymous function** functions with no name
+ * 
+ * **function inline**
+ * is a anonymous function declare in the call of other function
+ * - used with function callbacks that are probably not going to be reused elsewhere.
+ * - Used it when you know the function is not going to be reused
+ */
+// Function declaration that takes in two arguments: a function for displaying
+// a message, along with a name of a movie
+function movies(messageFunction, name) {
+    messageFunction(name);
+}
+
+// Call the movies function, pass in the function and name of movie
+movies(function displayFavorite(movieName) {
+    console.log("My favorite movie is " + movieName);
+}, "Finding Nemo");
+
