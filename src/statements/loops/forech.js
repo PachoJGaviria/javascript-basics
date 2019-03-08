93f85d9c614ff64126d307330db6fc437240d7aa
Francisco Javier Gaviria Sierra <fgaviria@psl.com.co>
2019-03-07 21:36:08 -0500

    Loops
    **Francisco Gaviria**

---
+++ src/statements/loops/forech.js
@@ -0,0 +1,9 @@
+/**
+ * ForEach
+ */
+var donuts = ["glazed", "chocolate frosted", "Boston creme", "glazed cruller"];
+donuts.forEach((element, index, array)=>{
+    console.log(`Element => ${element}`);
+    console.log(`Index   => ${index}`);
+    console.log(`array:  => ${array}`);
+});
