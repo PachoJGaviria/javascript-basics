4be374ea63679a4c92c8fbf0c91496931a114d9f
Francisco Javier Gaviria Sierra <fgaviria@psl.com.co>
2019-03-24 08:29:07 -0500

    Functions are First-Class Functions and Scope
    **Francisco Gaviria**

--- src/object/object.js
+++ src/object/object.js
@@ -1,14 +1,14 @@
 /**
- * Objects are one of the most important data structures in JavaScript. 
+ * Objects are one of the most important data structures in JavaScript.
  * Get ready to see them everywhere!
- * They have properties (information about the object) 
- * and methods (functions or capabilities the object has). 
- * Objects are an incredibly powerful data type and you will see them all over the place when working with JavaScript, 
+ * They have properties (information about the object)
+ * and methods (functions or capabilities the object has).
+ * Objects are an incredibly powerful data type and you will see them all over the place when working with JavaScript,
  * or any other object-oriented programming language.
- * 
+ *
  * distinct key/value pair, known as a property of that object,
  * objects are unordered collections
- * 
+ *
  * We can go ahead and remove that property from printer using the delete operator.
  */
 
@@ -25,8 +25,9 @@ var sister = {
   siblings: ["julia"],
   favoriteColor: "purple",
   pets: true,
-  paintPicture: () => { return "Sarah paints!"; }
-
+  paintPicture: () => {
+    return "Sarah paints!";
+  }
 };
 console.log(sister);
 // two equivalent ways to use the key to return its value
@@ -39,22 +40,22 @@ console.log(sister.paintPicture());
  * bad practices
  */
 var personWithBadNamesPractices = {
-    "name": "Piedad", // DON`T USE QUOTES IN THE OBJECT PROPERTY NAMES
-    "age": 57,
-    "1stChild": "Francisco", //BAD. DON`T USE A NUMBER HOW THE FIRST CHAR OF A PROPERTY
-    "2stChild": "Sara", //BAD. THE DOT NOTATION DON`T WORK WITH THOSE PROPERTIES
-    "favorite color": "red",//BAD. DON`T USE SPACES IN THE OBJECT PROPERTY NAMES
-    "favorite-country": "colombia"//BAD. DON`T USE HYPHENS
+  name: "Piedad", // DON`T USE QUOTES IN THE OBJECT PROPERTY NAMES
+  age: 57,
+  "1stChild": "Francisco", //BAD. DON`T USE A NUMBER HOW THE FIRST CHAR OF A PROPERTY
+  "2stChild": "Sara", //BAD. THE DOT NOTATION DON`T WORK WITH THOSE PROPERTIES
+  "favorite color": "red", //BAD. DON`T USE SPACES IN THE OBJECT PROPERTY NAMES
+  "favorite-country": "colombia" //BAD. DON`T USE HYPHENS
 };
 console.log("\n");
 console.log(personWithBadNamesPractices);
 var mom = {
-    name: "Piedad",
-    age: 57,
-    childrenNames: ["Francisco","Sara"],
-    favoriteColor: "red",
-    favoriteCountry: "Colombia"
-}
+  name: "Piedad",
+  age: 57,
+  childrenNames: ["Francisco", "Sara"],
+  favoriteColor: "red",
+  favoriteCountry: "Colombia"
+};
 console.log("\n");
 console.log(mom);
 console.log(mom["name"]);
@@ -63,12 +64,12 @@ console.log(mom.name);
 /**
  * In JavaScript, a primitive (e.g., a string, number, boolean, etc.) is immutable. In other words, any changes made to an argument inside a function effectively creates a copy local to that function, and does not affect the primitive outside of that function.
  * On the other hand, objects in JavaScript are mutable. If you pass an object into a function, Javascript passes a reference to that object.
- * Since objects are passed by reference, making changes to the copy (iceCreamCopy) has a direct effect on the original object (iceCreamOriginal) as well. 
+ * Since objects are passed by reference, making changes to the copy (iceCreamCopy) has a direct effect on the original object (iceCreamOriginal) as well.
  * **Comparing an Object with Another Object**
- * The expression will only return true when comparing two references to exactly the same object. 
+ * The expression will only return true when comparing two references to exactly the same object.
  * Objects are commonly created with literal notation, and can include properties that point to functions called methods. Methods are accessed the same way as other properties of objects, and can be invoked the same way as regular functions, except they automatically have access to the other properties of their parent object.
  * By default, objects are mutable (with a few exceptions), so data within them can be altered. New properties can be added, and existing properties can be modified by simply specifying the property name and assigning (or re-assigning) a value. Additionally, properties and methods of an object can be deleted as well with the delete operator, which directly mutates the object.
- * 
+ *
  * **Method and this**
  * A method is a function property of an object. It is accessed the same way as any other property of the object (i.e., using dot notation or square bracket notation), and is invoked the same way as a regular function outside of an object (i.e., adding parentheses to the end of the expression).
  * Since an object is a collection of data and the means to operate on that data, a method can access the object it was called on using the special this keyword. The value of this is determined when a method is invoked, and its value is the object on which the method was called. Since this is a reserved word in JavaScript, its value cannot be used as an identifier. Feel free to check out the links below for an additional look at methods and their relationship with this.
@@ -80,31 +81,74 @@ console.log(mom.name);
  * That function can be copied between objects.
  * When a function is called in the “method” syntax: object.method(), the value of this during the call is object.
  * Please note that arrow functions are special: they have no this. When this is accessed inside an arrow function, it is taken from outside.
- * 
+ *
  * **How the function is invoked determines the value of this inside the function.**
  * **When a regular function is invoked, the value of this is the global window object.**
- * 
+ *
  * Only declaring variables with the var keyword will add them to the window object. If you declare a variable outside of a function with either let or const, it will not be added as a property to the window object.
- * 
+ *
  * **Do not use globals variables or methods**
  * - Tight Coupling -> pieces of code are joined together in a way where changing one unintentionally alters the functioning of some other code
  * - Name Collisions -> A name collision occurs when two (or more) functions depend on a variable with the same name
  * - http://wiki.c2.com/?GlobalVariablesAreBad
- * 
+ *
  * **Object function**
  * - Object.keys() returns an array of a given object's own keys (property names).
  * - Object.values() returns an array of a given object's own values (property values).
- * 
- * 
+ *
+ *
  * **Functions are First-Class Functions**
  * - Be stored in variables
- * - Be returned from a function. -> **Higher-order function = A function that returns another function**
- * - Be passed as arguments into another function.
- * 
- * 
+ * - Be returned from a function. -> **Higher-order function = A function that returns another function or take it as argument**
+ * - Be passed as arguments into another function. -> **callback function**
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
  * https://javascript.info
  * https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Browser_support_for_JavaScript_APIs
  * http://crockford.com/javascript/
- * 
- * 
+ *
+ *
  */
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const numbersNames = [
+  "zero",
+  "one",
+  "two",
+  "three",
+  "four",
+  "five",
+  "six",
+  "seven",
+  "eight",
+  "nine"
+];
+// callback samples
+numbers.forEach(function callbackIsOddNumber(num) {
+  if (num % 2 !== 0) {
+    console.log(`The number ${num} is a odd number!`);
+  }
+});
+
+const numbersNamesLength = numbersNames.map(function callbackGetLength(word) {
+  return word.length;
+});
+console.log(`The names\`s length of the numbers between 0 and 9 are: ${numbersNamesLength}`);
+
+const evenNumbers = numbers.filter(function isEven(num) {
+  return num !== 0 && num % 2 === 0;
+});
+console.log(`The first even numbers are ${evenNumbers}`);
+
+
