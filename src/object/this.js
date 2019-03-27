1e9b43f43923e7b58b3afbf53c895d4fe5744945
Francisco Javier Gaviria Sierra <fgaviria@psl.com.co>
2019-03-27 06:50:54 -0500

    constructor and this
    **Francisco Gaviria**

---
+++ src/object/this.js
@@ -0,0 +1,16 @@
+/**
+ * When invoking a constructor function with the new operator, "this" gets set to the newly-created object!
+ * When you say "this" in a method, what you're really saying is "this object" or "the object at hand."
+ * When is this Assigned?
+ * The value of this is actually not assigned to anything until an object calls the method where this is used.
+ * The value assigned to "this" is based on the object that invokes the method where this is defined.
+ * **A common misconception is that "this" refers to the object where it is defined. This is not the case!**
+ */
+
+ /**
+  * There are four ways to call functions, and each way sets this differently.
+  * - Calling a constructor function with the new keyword sets this to a newly-created object.
+  * - Calling a function that belongs to an object (i.e., a method) sets this to the object itself.
+  * - Calling a function on its own (i.e., simply invoking a regular function) will set this to global scope. i.e the window, which is the global object if the host environment is the browser.
+  * - Call functions allows us to set "this" ourselves! -> with apply(), and with call(). Both methods share quite a few similarities, and they each allow us to specify how we want to set this
+  */
