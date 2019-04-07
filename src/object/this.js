1b69128ecf71213580b00592518bcf7449952e67
Francisco Javier Gaviria Sierra <fgaviria@psl.com.co>
2019-04-07 15:59:53 -0500

    ES6 notes
    **Francisco Gaviria**

--- src/object/this.js
+++ src/object/this.js
@@ -14,3 +14,29 @@
   * - Calling a function on its own (i.e., simply invoking a regular function) will set this to global scope. i.e the window, which is the global object if the host environment is the browser.
   * - Call functions allows us to set "this" ourselves! -> with apply(), call() and bind(). Those methods share quite a few similarities, and they each allow us to specify how we want to set this
   */
+
+  /**
+   * 1. A new object
+If the function is called with new:
+
+const mySundae = new Sundae('Chocolate', ['Sprinkles', 'Hot Fudge']); 
+In the code above, the value of this inside the Sundae constructor function is a new object because it was called with new.
+
+2. A specified object
+If the function is invoked with call/apply:
+
+const result = obj1.printName.call(obj2);
+In the code above, the value of this inside printName() will refer to obj2 since the first parameter of call() is to explicitly set what this refers to.
+
+3. A context object
+If the function is a method of an object:
+
+data.teleport();
+In the code above, the value of this inside teleport() will refer to data.
+
+4. The global object or undefined
+If the function is called with no context:
+
+teleport();
+In the code above, the value of this inside teleport() is either the global object or, if in strict mode, it's undefined.
+   */
