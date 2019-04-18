ea4746ca455856d7cc4ca8997b849acf5303c872
Francisco Javier Gaviria Sierra <fgaviria@psl.com.co>
2019-04-18 06:26:18 -0500

    Polyfills and Transpiling
    **Francisco Gaviria**

---
+++ src/browser compatibility/polyfills.js
@@ -0,0 +1,12 @@
+/**
+ * A **polyfill**, or polyfiller, is a piece of code (or plugin) that provides the technology that you, the developer, expect the browser to provide natively.
+ * There are polyfills for all sorts of browser features:
+ * SVG
+ * Canvas
+ * Web Storage (local storage / session storage)
+ * Video
+ * HTML5 elements
+ * Accessibility
+ * Web Sockets
+ * and many more!
+ */
---
+++ src/browser compatibility/transpiling.js
@@ -0,0 +1,9 @@
+/**
+ * It is a subset of compiler
+ * Take your code human readable to other spec code human readable
+ * i.e 
+ * ES6 code to ES5 code
+ * Java code to Javascript code
+ * Tools i.e:
+ * Babel
+ */
